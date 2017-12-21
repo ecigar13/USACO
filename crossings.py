87007c2e48f9b6b6617bb76c0919c59b9c5fba2c
Khoa Nguyen <khoanguyen0791@gmail.com>
2017-12-21 17:55:11 -0600

    Some changes.

--- crossings.py
+++ crossings.py
@@ -3,12 +3,20 @@ inFile=open('crossings.in','r')
 out=open('crossings.out','w')
 
 cows=int(inFile.readline())
-collided=[0 for i in range(cows)] #0= haven't checked, 1 = no collision, 2 = collide
-coordinates=[]
+collide=[]
+nocollide=[]
+
+## 2 arrays, move the item with no collision to nocollide
+## leave item with collision
+## item with collision will initially have all the cows
+## at the end, print items remaining in collision
+
 for i in range(cows):
     a=inFile.readline()
     start, stop = a.split()
-    coordinates.append((int(start),int(stop)))
+    collide.append((int(start),int(stop)))
+
+    
     
 for i in range(cows):
     if(collided[i]==1):
@@ -16,15 +24,16 @@ for i in range(cows):
     for j in range(cows):
         if(collided[j]==1 or j==i):
             continue;
-        diff1=coordinates[i][1]-coordinates[j][1]
-        diff2=coordinates[i][0]-coordinates[j][0]
+        diff1=collide[i][1]-collide[j][1]
+        diff2=collide[i][0]-collide[j][0]
         #print(diff1, diff2)
         diff1=math.copysign(1,diff1)
         diff2=math.copysign(1,diff2)
         if(diff1/diff2 < 0):
-            collided[i], collided[j]=2,2;
+            break
         else:
-            collided[i] = 1;
+            nocollide.append(collide.pop(i));
+        
 count=0;
 for i in collided:
     if(collided[i]==1):
